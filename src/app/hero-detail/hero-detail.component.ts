83f4b6e5ba9cbaf01c376be4e57d86bea05d0337
Urs Albisser <u.albisser@gmail.com>
2021-01-20 18:19:27 +0100

    Refactoring the original HeroesComponent into two components with a child HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';  // to import this component elsewhere
+
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+  // That's the only change you should make to the HeroDetailComponent class. There are no more properties. There's no presentation logic.
+  // This component simply receives a hero object through its hero property and displays it.
+
+
+  @Input() hero: Hero | undefined;  // allow hero to be imported elsewhere
 
   constructor() { }
 
