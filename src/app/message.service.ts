7b08abf4605c0644587bc3c24cea08b69ac4aaa1
Urs Albisser <u.albisser@gmail.com>
2021-01-21 19:53:15 +0100

    finish message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * The service exposes its cache of messages and two methods: one to add() a message to the cache and another to clear() the cache.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
+  public messages: string[] = [];
+
   constructor() { }
+
+  public add(message: string): void {
+    this.messages.push(message);
+  }
+
+  public clear(): void {
+    this.messages = [];
+  }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  public constructor(
+    // messageService property. Angular will inject the singleton MessageService into that property when it creates the MessagesComponent.
+    // The messageService property must be public because you're going to bind to it in the template.
+    public messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
   }
