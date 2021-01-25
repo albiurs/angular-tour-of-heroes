0a92a417e6fe0dfa9d83e9671e2b1ef15d14ac75
Urs Albisser <u.albisser@gmail.com>
2021-01-25 10:56:58 +0100

    finish dashboard component base

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
+
+/*
+The class is similar to the HeroesComponent class.
+    - It defines a heroes array property.
+    - The constructor expects Angular to inject the HeroService into a private heroService property.
+    - The ngOnInit() lifecycle hook calls getHeroes().
+
+This getHeroes() returns the sliced list of heroes at positions 1 and 5, returning only four of the Top Heroes (2nd, 3rd, 4th, and 5th).
+ */
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +18,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  private getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 
 }
