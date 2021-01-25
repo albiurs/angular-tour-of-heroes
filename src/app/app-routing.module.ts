ad1b9d43ca5ae27505d4d364acc2c1e90f8c3451
Urs Albisser <u.albisser@gmail.com>
2021-01-25 10:02:59 +0100

    add route for HeroesComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// Notice that the CommonModule references and declarations array are unnecessary, so are no longer part of AppRoutingModule. The following
+// sections explain the rest of the AppRoutingModule in more detail.
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'; // import RouterModule and Routes so the app can have routing functionality
+
+// HeroesComponent will give the Router somewhere to go once you configure the routes
+import { HeroesComponent } from './heroes/heroes.component';
+
+/*
+configure your routes
+Routes tell the Router which view to display when a user clicks a link or pastes a URL into the browser address bar.
+Since app-routing.module.ts already imports HeroesComponent, you can use it in the routes array:
+A typical Angular Route has two properties:
+    - path: a string that matches the URL in the browser address bar.
+    - component: the component that the router should create when navigating to this route.
+
+ */
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    // CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
